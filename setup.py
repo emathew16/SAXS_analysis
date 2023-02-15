52e464d8efc32866d559edf642c7cdf0b5b49cc3
emathew16 <88091762+emathew16@users.noreply.github.com>
2023-02-15 12:18:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pysaxs",
+    name="saxs_analysis",
     version="0.0.1",
     author="Elizabeth Mathew",
     author_email="Elizabeth.Mathew@hereon.de",
