7ce62adab0512fb8a61fb1f1e5f04e9fae25fdf7
emathew16 <88091762+emathew16@users.noreply.github.com>
2023-02-18 10:29:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="saxs_analysis",
-    version="0.0.5",
+    version="0.0.6",
     author="Elizabeth Mathew",
     author_email="Elizabeth.Mathew@hereon.de",
     description="SAXS python package",
@@ -13,9 +13,18 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/emathew16/SAXS_analysis",
     packages=setuptools.find_packages(),
+    py_modules=['saxspy']
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+)
+from setuptools import find_packages, setup
+
+setup(
+    name='mypkg',
+    version='0.1',
+    packages=find_packages(),
+    py_modules=['bacon']
 )
