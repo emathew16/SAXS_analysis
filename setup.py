f134e024330e3901ebf5be5b31134e327d6e72db
emathew16 <88091762+emathew16@users.noreply.github.com>
2023-02-18 10:35:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/emathew16/SAXS_analysis",
     packages=setuptools.find_packages(),
-    py_modules=['saxspy']
+    py_modules=['saxspy'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
